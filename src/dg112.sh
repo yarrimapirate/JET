ee306f2c37aef200108b918ebd3ceb2816539260
Donny Lynskey <donny@mtwe.biz>
2012-10-15 21:53:07 -0700

    v0.1 Alpha 1

--- src/dg112.sh
+++ src/dg112.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-#  112dg.sh
+#  112dg.sh - yarrimapirate@XDA v0.1
+#  
+#  A script to aqutomate the force QDL process for downgrading HBoot to 1.12
+#  on the HTC EVO 4G LTE.
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -18,8 +21,8 @@
 
 chmod +x ./adb 
 chmod +x ./fastboot 
-chmod +x ./emmc_recovery
-#chmod +x ./getbrickdrive.sh
+chmod +x ./emmc_recover
+
 
 clear
 
@@ -45,7 +48,7 @@ echo  "Preparing..."
 clear
 echo  "Phase 1"
 echo  ""
-echo  "This phase backs up /dev/mmcblk0p4 from your phone to this machine.  In" 
+echo  "This phase backs up /dev/block/mmcblk0p4 from your phone to this machine.  In" 
 echo  "addition, we will fetch your IMEI from the phone and use it to create an"
 echo  "additional partition 4 replacement to use as a failsafe.  From our"
 echo  "experience, as long as you have a valid partition 4 file (your backup" 
@@ -104,8 +107,6 @@ echo  "Now that we have backups, we're going to intentionally corrupt the"
 echo  "data on /dev/block/mmcblk0p4.  This will cause the phone to enter"
 echo  "Qualcomm download mode (or brick if you prefer)."
 echo  ""
-echo  "THE SCRIPT CURRENTLY ENDS HERE SO THIS IS A ONE WAY STREET!!"
-echo  ""
 echo  "It is strongly advised that you verify the presence of the files fsp4"
 echo  "and bakp4 in your working directory before continuing.  These are "
 echo  "critical in restoring your phone to operational status.  Do this in"
@@ -122,26 +123,75 @@ echo "Corrupting /dev/block/mmcblk0p4..."
 ./adb push ./killp4 /sdcard										# Load corrupt p4 file onto internal storage
 ./adb shell "dd if=/sdcard/killp4 of=/dev/block/mmcblk0p4"		# Flash corrupt p4 file
 ./adb shell "rm /sdcard/killp4"									# Clean up
+echo  ""
+echo  "Rebooting..."
+
 ./adb reboot													# Complete force QDL
 
 echo ""
 echo "Success."
 echo ""
 echo ""
-
 echo "Your phone should now appear to be off, with no charging light on."
-echo "You should now proceed with the steps listed on post 106 of the"
-echo "XDA thread starting after the blue *POOF*"
+echo ""
+echo  "From here forward, DO NOT UNPLUG THE PHONE FROM THE USB CABLE!"
 echo ""
 
+read -p "Press Enter to continue..."
+
+clear
+
+# QDL device detection
 
 lastdrive=$(ls -r /dev/sd? | sed 's\/dev/sd\\' | dd bs=1 count=1 2> /dev/null)	# Get all current /dev/sd* and filter to single letter of last drive
 ldascii=$(printf '%d\n' "'$lastdrive")											# Convert letter to ASCII value
 bdascii=$((ldascii+1))															# Increment ASCII Value, store in new variable
 brickdrive=$(printf \\$(printf '%03o' $bdascii))								# Convert brick drive ASCII value to character, store for later use
-printf "Your bricked phone should be accessible at /dev/sd$brickdrive\n\n"		# Print variable (testing)
 
+echo  "Now we'll flash the 1.12 bootloader."
+echo  ""
+printf  "If this process hangs at \"Waiting for /dev/sd"$brickdrive"12...\"  Press and\n"
+echo  "hold the power button on your phone for no less than 30 seconds and"
+echo  "then release it.  The process should wake back up a few seconds afterwards."
+echo  "Note that this process can take as long as 10 minutes to complete and you"
+echo  "will see a lot of repetitive output from the recovery tool."
 
-sudo modprobe -r qcserial							
-sudo mknod /dev/ttyUSB0 c 188 0
+echo  ""
+echo  "If this process fails to complete you will need to complete the manual steps"
+echo  "using the post on XDA.  In that case, your bricked phone should be"
+printf "accessible at /dev/sd$brickdrive\n\n"
 
+sudo modprobe -r qcserial																	# Reset qcserial kernel module and clear old blocks
+sudo mknod /dev/ttyUSB0 c 188 0																# Create block device for emmc_recover
+sudo ./emmc_recover -f hboot_1.12.0000_signedbyaa.nb0 -d /dev/sd"$brickdrive"12 -c 24576	# Flash Signed 1.12 HBoot
+
+echo  ""
+echo  "Successfully loaded HBoot v1.12!"
+echo  ""
+read -p "Press Enter to continue..." p
+
+clear
+
+echo  "The final step is restoring your backup /dev/block/mmcblk0p4."
+echo  ""
+printf  "Once again, if this process hangs at \"Waiting for /dev/sd"$brickdrive"4...\"\n"
+echo  "Press and hold the power button on your phone for no less than 30 seconds and"
+echo  "then release it.  The process should wake back up a few seconds afterwards."
+echo  ""
+echo  "If this process fails to complete you will need to complete the manual steps"
+echo  "using the post on XDA.  In that case, your bricked phone should be"
+printf "accessible at /dev/sd$brickdrive\n\n"
+
+sudo modprobe -r qcserial																	# Reset qcserial kernel module and clear old blocks
+sudo mknod /dev/ttyUSB0 c 188 0																# Create block device for emmc_recover
+sudo ./emmc_recover -f ./bakp4 -d /dev/sd"$brickdrive"4										# Flash backup p4 file
+
+echo ""
+echo  "Success!"
+echo  ""
+echo  "Your phone should now at least have a charging light on.  Some phones will"
+echo  "immediately boot after restoration.  If yours doesn't, simply unplug the USB"
+echo  "cable and hold your power button for a few seconds."
+echo  ""
+echo  "Enjoy HBoot 1.12!  You can now S-OFF with LazyPanda."
+echo  ""
