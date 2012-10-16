b4b5dda07d2336f3c9b64062d31faf85fd7158ef
closeone <closeonex@gmail.com>
2012-10-16 17:58:58 -0400

    Add more error-checking

--- src/dg112.sh
+++ src/dg112.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#  112dg.sh - yarrimapirate@XDA v0.1
+#  dg112.sh - yarrimapirate@XDA v0.1
 #  
 #  A script to aqutomate the force QDL process for downgrading HBoot to 1.12
 #  on the HTC EVO 4G LTE.
@@ -26,7 +26,6 @@ fi
 chmod +x ./adb 
 chmod +x ./fastboot 
 chmod +x ./emmc_recover
-
 
 clear
 
@@ -60,17 +59,17 @@ echo  "or the failsafe) it is not possible to irrevocably brick your device."
 echo  ""
 echo  "Please stand by..."
 echo  ""
+
 echo  "Rebooting to bootloader..."
 echo  ""
 
 ./adb reboot bootloader
-
-./fastboot getvar imei 2> imei2.txt				# Get IMEI from phone
 echo  "Getting IMEI value..."
-echo ""
+./fastboot getvar imei 2> imei2.txt				# Get IMEI from phone
+echo  ""
 
 echo  "Building failsafe P4 file..."
-echo ""
+echo  ""
 
 cat imei2.txt | grep "imei:" | sed s/"imei: "// > imei.txt	# Filter output to just number
 dd if=blankp4 bs=540 count=1 > ./fsp4 2> /dev/null		# First part of our failsafe P4 file
@@ -105,6 +104,24 @@ echo  "Pulling /dev/block/mmcblk0p4 backup from phone..."
 echo  ""
 
 ./adb shell "dd if=/dev/block/mmcblk0p4 of=/sdcard/bakp4"  	#  Copy P4 data to internal storage
+./adb shell "dd if=/dev/block/mmcblk0p4 of=/sdcard2/bakp4"  	#  Copy P4 data to external storage
+sdcardbak=$(./adb shell "if [ -e /sdcard2/bakp4 ]; then echo 1; fi") # Check that SD Card Backup was made
+if [ $sdcardbak != 1 ]; then
+	echo  "WARNING: A backup of your Partition 4 may not have been made on the SD Card."
+	echo  "Is an SD Card in your phone? Is it full?"
+	echo  "It's extremely recommended that a backup is made on an SD Card."
+	echo  ""
+	echo  "You can continue without making a backup, but it's recommended that you don't"
+	echo  "until this backup is made."
+	SDBackup(){
+	echo  "Continue without SD Card backup? [Y]es or [N]o?"
+		read BACKUP
+		case $BACKUP in
+		y | Y | yes | Yes) continue;;
+		n | N | no | No) exit;;
+		*) SDBackup;;
+		esac
+	}
 ./adb pull /sdcard/bakp4 ./bakp4							#  Pull file from internal storage to local machine
 
 if [ -e bakp4 ]; then										# Did the bakp4 get created?
@@ -117,6 +134,7 @@ s=0
 s=$(stat -c %s ./bakp4)									# Get size of bakp4  (Should be exactly 1024 bytes)
 if [ $s != 1024 ]; then									# Stop if size isn't right
 	echo  "FATAL:  Backup mmcblk0p4 size mismatch."
+	read -p ""
 	exit
 fi
 
@@ -135,11 +153,22 @@ echo  "and bakp4 in your working directory before continuing.  These are "
 echo  "critical in restoring your phone to operational status.  Do this in"
 echo  "another terminal window or in file manager."
 echo  ""
-echo  "LAST CHANCE.  If you need to exit this script press Ctrl-C now!"
+# echo  "LAST CHANCE.  If you need to exit this script press Ctrl-C now!"
 echo  ""
 
-read -p  "Press Enter to continue..." p
+# Prompt the user to answer the question. No Ctrl+C needed
+FinalQ() {
+echo "Are you sure you would like to continue? Once started, this cannot"
+echo "be cancelled. [Y]es or [N]o?"
+	read FINAL
+	case $FINAL in
+	y | Y | yes | Yes) continue;;
+	n | N | no | No) exit;;
+	*) FinalQ;;
+	esac
+}
 
+echo "Do NOT interrupt this process or reboot your computer."
 echo ""
 echo "Corrupting /dev/block/mmcblk0p4..."
 
@@ -165,12 +194,19 @@ read -p "Press Enter to continue..."
 clear
 
 # QDL device detection
-
+GetPart() {
 lastdrive=$(ls -r /dev/sd? | sed 's\/dev/sd\\' | dd bs=1 count=1 2> /dev/null)	# Get all current /dev/sd* and filter to single letter of last drive
 ldascii=$(printf '%d\n' "'$lastdrive")											# Convert letter to ASCII value
+# Test that the phone is actually recognized. Allow looping so the user can retry.
+if [ $ldascii == "0" ]; then
+	echo "For some reason, we can't detect your bricked phone."
+	echo "Unplug your phone, wait a few seconds, and plug it back in."
+	read -p "Press Enter to retry detection."
+	GetPart
+fi
 bdascii=$((ldascii+1))															# Increment ASCII Value, store in new variable
 brickdrive=$(printf \\$(printf '%03o' $bdascii))								# Convert brick drive ASCII value to character, store for later use
-
+}
 echo  "Now we'll flash the 1.12 bootloader."
 echo  ""
 printf  "If this process hangs at \"Waiting for /dev/sd"$brickdrive"12...\"  Press and\n"
@@ -180,7 +216,7 @@ echo  "Note that this process can take as long as 10 minutes to complete and you
 echo  "will see a lot of repetitive output from the recovery tool."
 
 echo  ""
-echo  "If this process fails to complete you will need to complete the manual steps"
+echo  "If this process fails to complete, you will need to complete the manual steps"
 echo  "using the post on XDA.  In that case, your bricked phone should be"
 printf "accessible at /dev/sd$brickdrive\n\n"
 
@@ -189,11 +225,14 @@ sudo mknod /dev/ttyUSB0 c 188 0																# Create block device for emmc_re
 sudo ./emmc_recover -f hboot_1.12.0000_signedbyaa.nb0 -d /dev/sd"$brickdrive"12 -c 24576	# Flash Signed 1.12 HBoot
 
 echo  ""
-echo  "Successfully loaded HBoot v1.12!"
+echo  "Successfully loaded HBOOT 1.12.0000!"
 echo  ""
-read -p "Press Enter to continue..." p
+# Just move straight into the last step. Probably better to be as quick as possible with this?
+#read -p "Press Enter to continue..." p
 
 clear
+
+echo "Unbricking"
 
 echo  "The final step is restoring your backup /dev/block/mmcblk0p4."
 echo  ""
@@ -209,12 +248,12 @@ sudo modprobe -r qcserial																	# Reset qcserial kernel module and cle
 sudo mknod /dev/ttyUSB0 c 188 0																# Create block device for emmc_recover
 sudo ./emmc_recover -f ./bakp4 -d /dev/sd"$brickdrive"4										# Flash backup p4 file
 
-echo ""
+echo  ""
 echo  "Success!"
 echo  ""
 echo  "Your phone should now at least have a charging light on.  Some phones will"
 echo  "immediately boot after restoration.  If yours doesn't, simply unplug the USB"
 echo  "cable and hold your power button for a few seconds."
 echo  ""
-echo  "Enjoy HBoot 1.12!  You can now S-OFF with LazyPanda."
+echo  "Enjoy HBOOT 1.12!  You can now S-OFF with LazyPanda."
 echo  ""
